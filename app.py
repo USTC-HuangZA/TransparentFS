0c8bb505b7ab386748851cc2a071f08b7f6cb5df
USTC-HuangZA <30893523+USTC-HuangZA@users.noreply.github.com>
2021-04-14 14:59:32 +0800

    clear_all enable

--- app.py
+++ app.py
@@ -136,4 +136,4 @@ def demo2():
     return jsonify({'items': bitmap_hist})
 
 if __name__ == '__main__':
-     app.run(host="0.0.0.0", port=5000, debug=True)
+     app.run(host="0.0.0.0", port=80, debug=False)
